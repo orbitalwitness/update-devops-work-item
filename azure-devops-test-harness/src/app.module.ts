23ae545b0fa5e49bf4eec0e651d3a11a34fe396a
Steve Doggett <artigat1@gmail.com>
2022-10-16 11:04:49 +0100

    Added swagger and renamed some files from the initial install

--- azure-devops-test-harness/src/app.module.ts
+++ azure-devops-test-harness/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AzureDevOpsService } from './azure-devops.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AzureDevOpsService],
 })
 export class AppModule {}
