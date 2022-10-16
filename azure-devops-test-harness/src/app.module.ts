ac6ab665462effd181d36b76bee46e1bf4674448
Steve Doggett <artigat1@gmail.com>
2022-10-16 11:06:55 +0100

    Add in the config package for use with .env
    The .env file is not included

--- azure-devops-test-harness/src/app.module.ts
+++ azure-devops-test-harness/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AzureDevOpsService } from './azure-devops.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AzureDevOpsService],
 })
