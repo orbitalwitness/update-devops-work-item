ba1548e4b8d106588b167c8c010ab57e3178c78a
Steve Doggett <steve@orbitalwitness.com>
2022-11-02 16:08:35 +0000

    mismatch on input names

--- src/helpers.js
+++ src/helpers.js
@@ -7,16 +7,19 @@ const core = require('@actions/core')
  *     (string|string), gh_token: (*|string)}}}
  */
 export const getValuesFromPayload = (payload) => {
+    const organisation = core.getInput('organisation', { required: true })
+    const url = `https://dev.azure.com/${ organisation }`
+
     const env = {
         action: payload.action !== undefined ? payload.action : '',
         env: {
-            organisation: core.getInput('ado_organisation', { required: true }),
-            org_url: `https://dev.azure.com/${ core.getInput('ado_organisation') }`,
             ado_token: core.getInput('ado_token', { required: true }),
-            gh_repo_owner: core.getInput('gh_repo_owner', { required: true }),
-            gh_repo: core.getInput('gh_repo', { required: true }),
-            pull_number: core.getInput('pull_number', { required: true }),
             gh_token: core.getInput('gh_token', { required: true }),
+            organisation,
+            org_url: url,
+            gh_repo_owner: core.getInput('gh_repo_owner', { required: false }) ?? '',
+            gh_repo: core.getInput('gh_repo', { required: false }) ?? '',
+            pull_number: core.getInput('pull_number', { required: true }),
             new_state: core.getInput('new_state', { required: true }),
             description: core.getInput('description') ?? '',
             closed_state: core.getInput('closed_state') ?? 'Closed'
