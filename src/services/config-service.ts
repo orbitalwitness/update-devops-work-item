f5f3d561485dc5e0806ede312331ea24d7edd7cd
Steve Doggett <artigat1@gmail.com>
2022-11-11 16:34:26 +0000

    didn't work, so using getInput again

--- src/services/config-service.ts
+++ src/services/config-service.ts
@@ -1,3 +1,4 @@
+import { getInput } from "@actions/core";
 import { IConfigService } from "../interfaces/config-service.interface";
 import { IEnvironment } from "../interfaces/environment.interface";
 
@@ -5,21 +6,21 @@ export class ConfigService implements IConfigService {
   private readonly env: IEnvironment;
 
   constructor(githubContext: any) {
-    const organisation = process.env["organisation"] ?? "";
+    const organisation = getInput("organisation", { required: true });
     const url = `https://dev.azure.com/${organisation}`;
 
     this.env = {
       action: githubContext.action !== undefined ? githubContext.action : "",
-      adoToken: process.env["ado_token"] ?? "",
-      ghToken: process.env["gh_token"] ?? "",
+      adoToken: getInput("ado_token", { required: true }),
+      ghToken: getInput("gh_token", { required: true }),
       organisation,
       orgUrl: url,
-      ghRepoOwner: process.env["gh_repo_owner"] ?? "",
-      ghRepo: process.env["gh_repo"] ?? "",
-      pullNumber: Number(process.env["pull_number"] ?? ""),
-      newState: process.env["new_state"] ?? "",
-      description: process.env["description"] ?? "",
-      closedState: process.env["closed_state"] ?? "Closed",
+      ghRepoOwner: getInput("gh_repo_owner", { required: false }) ?? "",
+      ghRepo: getInput("gh_repo", { required: false }) ?? "",
+      pullNumber: Number(getInput("pull_number", { required: true })),
+      newState: getInput("new_state", { required: true }),
+      description: getInput("description") ?? "",
+      closedState: getInput("closed_state") ?? "Closed",
     };
   }
 
